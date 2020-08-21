c74d256f8ae1366437f9afbf8acdaf030821f7d6
kimcore <kimcoreovo@gmail.com>
2020-08-21 22:13:19 +0900

    1.0.0

---
+++ README.md
@@ -0,0 +1,40 @@
+# simpleyt
+
+[![image](https://img.shields.io/github/license/kimcore/simpleYT?style=flat-square)](https://github.com/kimcore/simpleYT/blob/master/LICENSE)
+
+[![image](https://nodei.co/npm/simpleyt.png?downloads=true&stars=true)](https://nodei.co/npm/simpleyt/)
+
+> Simple youtube search library without need of an api key
+
+## Usage
+```js
+const simpleYT = require('simpleyt')
+
+simpleYT('JUSTHIS', {
+    filter: 'video', // Filter only videos
+    localAddress: '2001:470:1234:5678::a'
+    // Other parameters are passed to axios
+}).then(console.log)
+```
+## Result
+```js
+[
+  {
+    type: 'video',
+    identifier: 'iMTblJbmam4',
+    uri: 'https://www.youtube.com/watch?v=iMTblJbmam4',
+    title: "[ENG/CC][4K] JUSTHIS(저스디스)의 킬링벌스를 라이브로! I IndiGO, Diablo, Gone, DF KV Freestyle, That Ain't Real 등",
+    author: {
+      name: 'dingo freestyle',
+      profile: 'https://yt3.ggpht.com/a-/AOh14GiiLlR1duwARrL4IWqmVVZcuH-mdVnQLunfTw=s68-c-k-c0x00ffffff-no-rj-mo',
+      uri: 'https://www.youtube.com/user/nunnaamo'
+    },
+    length: { ms: 794000, sec: 794 },
+    isStream: false,
+    thumbnails: [ [Object], [Object], [Object], [Object] ]
+  },
+  ...
+]
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,69 @@
+const axios = require('axios')
+
+module.exports = async (query, options) => {
+    options = options ? options : {}
+    const requestOptions = options ? JSON.parse(JSON.stringify(options)) : null
+    requestOptions['filter'] = undefined
+    const response = (await axios.get(
+        'https://www.youtube.com/results?search_query=' + encodeURIComponent(query), requestOptions
+    )).data
+    const line = response.match(/window\["ytInitialData"]\s*=\s*(.*);+\n/)[0]
+    const json = JSON.parse(line.substring(line.indexOf('{'), line.length - 2))
+    const result = json
+        ['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']
+        ['contents'][0]['itemSectionRenderer']['contents']
+    return result.filter(video => {
+        const type = Object.keys(video)[0].replace('Renderer', '')
+        return ['video', 'playlist'].includes(type)
+    }).map(video => {
+        const type = Object.keys(video)[0].replace('Renderer', '')
+        const data = video[type + 'Renderer']
+        const identifier = data[type + 'Id']
+        if (type === 'video') {
+            const isStream = !Object.keys(data).includes('lengthText')
+            let length = Number.MAX_VALUE
+            if (!isStream) {
+                length = 0
+                data['lengthText']['simpleText'].split(':').reverse().forEach((value, index) => {
+                    const i = Number(value)
+                    length += (index === 0 ? i : i * (60 ** index))
+                })
+            }
+            return {
+                type: type,
+                identifier: identifier,
+                uri: 'https://www.youtube.com/watch?v=' + identifier,
+                title: data['title']['runs'][0]['text'],
+                author: {
+                    name: data['ownerText']['runs'][0]['text'],
+                    profile: data['channelThumbnailSupportedRenderers']['channelThumbnailWithLinkRenderer']
+                        ['thumbnail']['thumbnails'][0]['url'],
+                    uri: 'https://www.youtube.com' + data['ownerText']['runs'][0]['navigationEndpoint']
+                        ['commandMetadata']['webCommandMetadata']['url']
+                },
+                length: {
+                    ms: isStream ? length : length * 1000,
+                    sec: length
+                },
+                isStream: isStream,
+                thumbnails: data['thumbnail']['thumbnails']
+            }
+        } else return {
+            type: type,
+            identifier: identifier,
+            uri: 'https://www.youtube.com/playlist?list=' + identifier,
+            title: data['title']['simpleText'],
+            author: {
+                name: data['longBylineText']['runs'][0]['text'],
+                uri: 'https://www.youtube.com' + data['longBylineText']['runs'][0]['navigationEndpoint']
+                    ['commandMetadata']['webCommandMetadata']['url']
+            },
+            count: Number(data['videoCount']),
+            thumbnails: data['thumbnails']
+        }
+    }).filter(it => {
+        if (options.filter === 'video') return it.type === 'video'
+        else if (options.filter === 'playlist') return it.type === 'playlist'
+        else return true
+    })
+}
